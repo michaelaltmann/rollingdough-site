ddfb235caac8a484becae4c188125ffe798c4bf4
maltmann <michael.altmann@gmail.com>
2022-04-20 13:47:08 -0500

    More bakeries

--- src/models/bakery-store/bakery-store.ts
+++ src/models/bakery-store/bakery-store.ts
@@ -2,7 +2,24 @@ export const BakeryStoreModel = {
 
   getBakeries() {
     return [
+      {name: "Laune Breads"},
+      {name: "Black Walnut"},
+      {name: "Marc Heu Patisserie Paris"},
+      {name: "Sun Street Breads"},
+      {name: "Isle Buns"},
+      {name: "Sift"},
+      {name: "Honey and Rye"},
+      {name: "Savory Bake House"},
+      {name: "Pie and Mighty"},
+      {name: "Great Harvest"},
+      {name: "Butter"},
+      {name: "Rose Street"},
+      {name: "El Mexicano"},
+      {name: "Asa's Bakery"},
       {name: "Rustica"},
+      {name: "Halwo Kismayo Bakery"},
+      {name: "Glam Doll Doughnuts"},
+      {name: "Bogart Doughnuts"},
       {name: "Patisserie 46"},
       {name: "Turtle Bread"}
     ]
--- src/screens/bakeries/bakeries-screen.tsx
+++ src/screens/bakeries/bakeries-screen.tsx
@@ -2,7 +2,7 @@ import { BakeryStoreModel } from "../../models/bakery-store/bakery-store";
 import Grid from "@mui/material/Grid";
 export const BakeriesScreen = () => {
   const items = BakeryStoreModel.getBakeries();
-
+  items.sort((x, y) => x.name.localeCompare(y.name));
   return (
     <Grid container direction="column">
       {" "}
